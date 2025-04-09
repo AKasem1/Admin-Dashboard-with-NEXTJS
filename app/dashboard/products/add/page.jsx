b3006c84b945ca3723c02beb03d5b0e08cd6c6e9
AKasem1 <abdelrahmankasem7@gmail.com>
2025-04-09 06:33:22 +0200

    Users Page, Products Page, and User Details

---
+++ app/dashboard/products/add/page.jsx
@@ -0,0 +1,30 @@
+import styles from '@/app/ui/dashboard/products/addProduct/addProductPage.module.css';
+
+const AddProductPage = () => {
+    return (
+      <div className={styles.container}>
+        <form className={styles.form} action="">
+          <input placeholder='title' name='title' type='text' required />
+          <select name='cat' required id='cat'>
+            <option value="general">Choose a Category</option>
+            <option value="kitchen">Kitchen</option>
+            <option value="phone">Phone</option>
+            <option value="computer">Computer</option>
+          </select>
+          <input placeholder='price' name='price' type='number' required />
+          <input placeholder='stock' name='stock' type='number' required />
+          <input placeholder='color' name='color' type='text' required />
+          <input placeholder='size' name='size' type='text' required />
+          <textarea 
+          placeholder='Description' 
+          name='desc' 
+          required
+          id='desc'
+          rows={16}
+          ></textarea>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+export default AddProductPage;
--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,6 +1,60 @@
+import Search from '@/app/ui/dashboard/search/search';
+import styles from '../../ui/dashboard/products/products.module.css';
+import Link from 'next/link';
+import Pagination from '@/app/ui/dashboard/pagination/pagination';
+import Image from 'next/image';
+
 const ProductsPage = () => {
-    return (
-      <div>ProductsPage</div>
-    );
-  }
-  export default ProductsPage;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder={"Search for Products.."} />
+        <Link href="/dashboard/products/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created At</td>
+            <td>Stock</td>
+            <td>Actions</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.product}>
+                <Image
+                  src="/noproduct.jpg"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.productImage}
+                />
+                IPhone
+              </div>
+            </td>
+            <td>Desc</td>
+            <td>$999</td>
+            <td>13.01.2022</td>
+            <td>72</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href='/dashboard/products/test'>
+                  <button className={`${styles.button} ${styles.view}`}>View</button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
+}
+export default ProductsPage;
